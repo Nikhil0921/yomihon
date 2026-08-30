5681853e1ad4c3141cfbe3ed384272f4f43c7a4d
Nikhil Kumar <nikhil0921nine@gmail.com>
2026-08-30 13:42:34 +0000

    chore(release): bump version to 0.5.0 (versionCode 26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     defaultConfig {
         applicationId = "app.yomihon"
 
-        versionCode = 25
-        versionName = "0.4.0"
+        versionCode = 26
+        versionName = "0.5.0"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
